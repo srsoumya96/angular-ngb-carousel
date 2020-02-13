d3f2c515b8ccc2c4397650a2d35f7cfa65c9de99
srsoumya96 <sr.soumya96@gmail.com>
2020-02-13 11:35:07 +0530

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';  
+
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './carousel/carousel.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule, NgbModule ],
+  declarations: [ AppComponent, CarouselComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
